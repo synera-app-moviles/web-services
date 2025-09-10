3987d484b6b87162b00a51e3c56e8ad91ab2f66e
Neilcuri7 <u20231b866@upc.edu.pe>
2025-09-10 10:48:19 -0500

    initialize Spring Boot web services project with Maven wrapper and basic configuration

---
+++ src/test/java/synera/centralis/api/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package synera.centralis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
